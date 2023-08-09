891253512b5e7313e02fef4d626c5ce61c5704d9
typicalfeline <140970400+typicalfeline@users.noreply.github.com>
2023-08-09 16:03:09 +0200

    add docker installation

--- scripts/install-poetry.sh
+++ scripts/install-poetry.sh
@@ -5,5 +5,4 @@ curl -sSL https://install.python-poetry.org | python3 -
 ##
 # Add poetry to PATH in Oh My Zsh
 #
-cd $ZSH_CUSTOM
-echo "export PATH=$PATH:~/.local/bin" >> add_poetry_path.zsh
+echo "export PATH=$PATH:~/.local/bin" >> ${ZSH_CUSTOM}/add_poetry_path.zsh
